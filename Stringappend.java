ddda580033d487ff7a11f7b870a3230f1e8b4c4c
KannanVasu <39426261+KannanVasu@users.noreply.github.com>
2018-07-11 09:46:10 +0530

    Create Stringappend.java

---
+++ Stringappend.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Stringappend
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    
+    System.out.print(s+".");
+    
+  }
+}
