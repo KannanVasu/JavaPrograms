ceb1b14bdc6afa62256457c4dfbecab880fe1165
KannanVasu <39426261+KannanVasu@users.noreply.github.com>
2018-07-29 19:14:56 +0530

    Create FirstRepatedNumber.java

---
+++ FirstRepatedNumber.java
@@ -0,0 +1,44 @@
+package ww;
+import java.util.*;
+import java.util.Scanner;
+
+public class FirstRepatedNumber{
+
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int[] a=new int[n];
+	for(int i=0; i<n; i++)
+	{
+		a[i]=sc.nextInt();
+	}
+	int c=0;
+	String s="",t="";
+	for(int i=0; i<n; i++)
+	{ t=String.valueOf(a[i]+" ");
+		if(!s.contains(t))
+		{
+			s=s+t;
+		}
+	}
+	String t2[]=s.split(" ");
+	for(int i=0; i<t2.length; i++)
+	{
+		int a2=Integer.valueOf(t2[i]);
+		for(int j=0; j<n; j++)
+		{
+			if(a2==a[j])
+				c=c+1;
+		}
+		if(c>1)
+		{
+			System.out.print(a2+" ");
+			break;
+		}
+		
+		c=0;
+	}
+}
+
+}
